60460fc6c2271a0bf84341a02e5ea494e355e3a1
Matias M. Montenegro <matiasmmontenegro@gmail.com>
2015-08-17 03:35:05 -0300

    Added socket.io library and configured it to receive packages

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
+"use strict";
+
 var tcpEventEmitter = require('./lib/tcpEventEmitter'),
     appServer = require('./lib/server');
 
 
 tcpEventEmitter.on('tcp.recv', function(tcpSession) {
-    console.log(tcpSession);
-});
-
-
-
-tcpEventEmitter.on('tcp.recv', function(tcpSession) {
-    console.log(tcpSession);
+    appServer.sendSession(tcpSession);
 });
